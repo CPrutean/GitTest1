beed5d7a92edbbc1c2b94e3e66a8aa540615ab4c
pruteanc <107134403+CPrutean@users.noreply.github.com>
2022-06-10 16:20:21 +0000

    firewall bypass functionality added

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ while not runCode == "y":
   runCode = input("Confirm initiation: ")
 ipAddress = input("Target IP Address: ")
 
-slines = ["system check -- passed", "launch protocol tcp_8005", "attempt winver check -- passed", "reorder required packets -- complete", "intcheck passed", "waiting for response from 192.168.0.102", "host shell32 error", "prepare program found at C:\hpswsetup\sp115284\setup.exe", "perform handshake with target host"]
+slines = ["system check -- passed", "launch protocol tcp_8005", "attempt winver check -- passed", "reorder required packets -- complete", "intcheck passed", "waiting for response from 192.168.0.102", "host shell32 error", "prepare program found at C:\hpswsetup\sp115284\setup.exe", "perform handshake with target host", "firewall bypassed"]
 
 flines = ["cmd_check1","cmd_check2", "cmd_check42", "cmd_check9", "cmd_check13", "cmd_check5", "cmd_check44", "cmd_check33", "cmd_check15", "cmd_check63", "cmd_check0", "mac_address_error -- autoresolve", "prompt readout void", "error at C:\ProgramData\regid.1991-06.com.microsoft\regid.1991-06.com.microsoft_Windows-10-Home.swidtag", "intel process test validated", "varied response length timeout"]
 
@@ -18,7 +18,6 @@ for i in range(42):
     print(flines[random.randint(0,(len(flines)-1))])
     i=i+1
     time.sleep(0.01)
-print("hi")
 print("\n")
 runCode = input("Install required files? ")
 print("Installing...")
